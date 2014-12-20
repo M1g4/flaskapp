f1280f8848fea56076993cd55b29a804c019fedc
Sebastian Lammel <sebastianlammel@googlemail.com>
2014-12-20 22:00:06 +0100

    add posts

--- blog.py
+++ blog.py
@@ -11,8 +11,10 @@ app = Flask(__name__)
 
 app.config.from_object(__name__)
 
+
 def connect_db():
-    return sqlite3.connect(app.config['DATABSE'])
+    return sqlite3.connect(app.config['DATABASE'])
+
 
 def login_required(test):
     @wraps(test)
@@ -20,21 +22,23 @@ def login_required(test):
         if 'logged_in' in session:
             return test(*args, **kwargs)
         else:
-            flash('You need to login first.')
+            flash ('You need to login first')
             return redirect(url_for('login'))
     return wrap
+
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
         if request.form['username'] != app.config['USERNAME'] or \
-            request.form['password'] != app.config['PASSWORD']:
+                request.form['password'] != app.config['PASSWORD']:
             error = 'Invalid Credentials. Please try again.'
         else:
-            session['loged_in'] = True
+            session['logged_in'] = True
             return redirect(url_for('main'))
     return render_template('login.html', error=error)
+
 
 @app.route('/logout')
 def logout():
@@ -42,10 +46,32 @@ def logout():
     flash('You were logged out')
     return redirect(url_for('login'))
 
+
 @app.route('/main')
 @login_required
 def main():
-    return render_template('main.html')
+    g.db = connect_db()
+    cur = g.db.execute('select * from posts')
+    posts = [dict(title=row[0], post=row[1]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template('main.html', posts=posts)
+
+
+@app.route('/add', methods=['POST'])
+@login_required
+def add():
+    title = request.form['title']
+    post = request.form['post']
+    if not title or not post:
+        flash("All fields are required. Please fill in 'title' and 'post'")
+        return redirect(url_for('main'))
+    else:
+        g.db = connect_db()
+        g.db.execute('insert into posts (title, post) values (?, ?)', [request.form['title'], request.form['post']])
+        g.db.commit()
+        g.db.close()
+        flash('New entry was successfully posted!')
+        return redirect(url_for('main'))
 
 if __name__ == '__main__':
     app.run(debug=True)
